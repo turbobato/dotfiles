f42ad8f4d45a6954151f34d4c82e3d16022acece
Guilhem JAZERON <guilhem.jazeron@telecom-paris.fr>
2022-09-20 17:56:19 +0200

    ajout de .zshrc, .i3

--- install_scripts/install.sh
+++ install_scripts/install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-# install zsh and oh-my-zsh
+# Install zsh and oh-my-zsh
 pacman -Syu zsh
 chsh -s $(which zsh) "$1"
 su -c "cd install_scripts; bash install_oh_my_zsh.sh" "$1"
-# disable beep sound
+# Disable beep sound
 rmmod pcspkr
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+# Setup for screenshots
+pacman -Syu flameshot
+mkdir "/home/$1/Images/screenshots"
+# Install xidlehook
+git clone https://aur.archlinux.org/xidlehook.git
+cd xidlehook
+makepkg -si
+# Install iosevka font
+pacman -Syu ttf-iosevka-nerd
